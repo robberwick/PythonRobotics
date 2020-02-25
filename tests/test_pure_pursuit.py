5d44ba93684f907c7364f35c03388e84f74aabf7
markmellors <mark.mellors@gmail.com>
2020-02-25 17:30:39 +0000

    added a test case to check index returned by Trajectory.search_target_index

--- tests/test_pure_pursuit.py
+++ tests/test_pure_pursuit.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
-from PathTracking.pure_pursuit import pure_pursuit as m
+import pure_pursuit as m
 
 print("pure_pursuit test")
+
 
 
 class Test(TestCase):
@@ -9,3 +10,23 @@ class Test(TestCase):
     def test1(self):
         m.show_animation = False
         m.main()
+
+    def test2(self):
+        cx = [0,1,2,3]  #needs at least three points, and at least one outside the look-ahead distance
+        cy = [0,0,0,0]
+        testPath = m.Trajectory(cx,cy)
+        #start at origin, not moving
+        state = m.State(x=-0.0, y=0, yaw=0.0, v=0.0)
+        #could check initialIndex was correct if look-ahead distance (Lfc) was accessible
+        initialIndex = testPath.search_target_index(state)
+
+        #asking again shouldn't change the answer:
+        iteratedIndex = testPath.search_target_index(state)
+        iteratedIndex = testPath.search_target_index(state)
+        if (initialIndex != iteratedIndex):
+            raise Exception("incorrect index returned by Trajectory.search_target_index")
+
+Thistest = Test()
+Thistest.test1()
+Thistest.test2()
+print("done")
