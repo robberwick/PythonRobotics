b0b6d206a906dc8aba4aa34d72b74654983d9219
Rob Berwick <rob.berwick@gmail.com>
2020-02-25 19:11:50 +0000

    Reformat pure_pursuit test

--- tests/test_pure_pursuit.py
+++ tests/test_pure_pursuit.py
@@ -1,32 +1,35 @@
 from unittest import TestCase
-import pure_pursuit as m
+from PathTracking.pure_pursuit import pure_pursuit as m
 
-print("pure_pursuit test")
-
+print(__file__)
 
 
 class Test(TestCase):
-
     def test1(self):
         m.show_animation = False
         m.main()
 
     def test2(self):
-        cx = [0,1,2,3]  #needs at least three points, and at least one outside the look-ahead distance
-        cy = [0,0,0,0]
-        testPath = m.Trajectory(cx,cy)
-        #start at origin, not moving
+        cx = [0, 1, 2, 3]  # needs at least three points, and at least one outside the look-ahead distance
+        cy = [0, 0, 0, 0]
+        testPath = m.Trajectory(cx, cy)
+        # start at origin, not moving
         state = m.State(x=-0.0, y=0, yaw=0.0, v=0.0)
-        #could check initialIndex was correct if look-ahead distance (Lfc) was accessible
+        # could check initialIndex was correct if look-ahead distance (Lfc) was accessible
         initialIndex = testPath.search_target_index(state)
 
-        #asking again shouldn't change the answer:
+        # asking again shouldn't change the answer:
         iteratedIndex = testPath.search_target_index(state)
         iteratedIndex = testPath.search_target_index(state)
-        if (initialIndex != iteratedIndex):
-            raise Exception("incorrect index returned by Trajectory.search_target_index")
+        self.assertEqual(
+            initialIndex,
+            iteratedIndex,
+            msg="incorrect index returned by Trajectory.search_target_index"
+        )
 
-Thistest = Test()
-Thistest.test1()
-Thistest.test2()
-print("done")
+
+if __name__ == '__main__':  # pragma: no cover
+    test = Test()
+    test.test1()
+    test.test2()
+
